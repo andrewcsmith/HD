a703fcc43985b7ce62d20c914964b71919c0b020
andrewcsmith <andrewchristophersmith@gmail.com>
2012-12-01 10:49:36 -0500

    Alpha! This HD Module is now somewhat commented up, and seems to be quite stable.

--- hd.rb
+++ hd.rb
@@ -2,9 +2,6 @@
 # ==A module for measuring harmonic distance in just intonation
 # 
 # 
-# 
-# 
-# 
 # This module takes much of its inspiration from two sources: 
 # 
 # The more general theoretical source is James Tenney's measurement of
@@ -33,7 +30,8 @@ module HD
   # musical compositions, and using a smaller list of primes makes processing
   # easier (since the list is iterated over often).
   # 
-  # Changing the constant is simple, but must be deliberate:
+  # Changing the constant is simple:
+  # 
   #   module HD
   #     PRIMES = ::NArray[...]
   #   end
@@ -55,16 +53,19 @@ module HD
   # Holds the configuration parameters for the various HD measurement
   # functions.
   # 
-  # Settings that are possible as of now: a custom list of prime number
-  # weights, and a custom filename from which to read in a list of tuneable
-  # intervals.
+  # Options:
+  # 
+  # [+:prime_weights+]  How much each "city block" is weighted in the particular metric space.
+  # [+:pc_only+]        A convenient way of setting the weighting of prime 2 to 1
+  # [+:tuneable_file+]  Location of a file with the list of tuneable intervals
+  # 
   class HDConfig
     attr_accessor :pc_only, :prime_weights, :tuneable
     
     # Creates a new HDConfig object with the following default options:
     # * pc_only: false
     # * prime_weights: [2,3,5,7,11,13,17,19,23]
-    # *  
+    # * tuneable: List from Marc Sabat's "Analysis of Tuneable Intervals on Violin and Cello"
     def initialize(options = { })
       @options = options
       @prime_weights =    options[:prime_weights]   || PRIMES.dup
@@ -95,6 +96,7 @@ module HD
       end
     end
     
+    # Setter for [+:prime_weights+]. Any unspecified weights past the highest value in the setter array are set to 0.
     def prime_weights=(new_weights)
       if new_weights.size != PRIMES.size
         PRIMES.size.times do |i|
@@ -106,6 +108,7 @@ module HD
       @prime_weights = ::NArray.to_na(new_weights)
     end
     
+    # Rejects all intervals that are outside of the space of tuneable intervals
     def reject_untuneable_intervals!
       self.tuneable.reject! {|x| (x.distance(HD.r, self) ** -1) == 0}
     end
@@ -116,20 +119,29 @@ module HD
     
   end # HDConfig (class)
   
-  # Ratio class, which defines a point in harmonic space.
-  # In the process of re-writing this so it is a 2D NVector
+  # Ratio class, which defines a point in harmonic space. A Ratio is a
+  # 2-dimensional NVector where the first dimension is numerator and second is
+  # denominator. Create a new Ratio (in the general namespace) with: 
+  # 
+  #   HD::Ratio[x, y]
+  # 
+  # or with the simpler shortcut:
+  # 
+  #   HD.r(x, y)
+  # 
+  # A newly created Ratio is automatically reduced to its lowest common terms.
+  # 
+  # The Rational class has not been used as a superclass, because the
+  # succession of NVectors translates very well into a 3-dimensional NArray
+  # when operating on a vector of Ratio objects.
   class Ratio < NVector
-    # attr_reader :num, :den
     include Enumerable
-    require 'rational'
+    # require 'rational'
     
-    # Default value is 1/1. This make it easier to provide an origin of 1/1 for any distance function.
+    # Default value is 1/1. This make it easier to provide an origin of 1/1
+    # for any distance function.
     def initialize(*args)
-      # if args.is_a? NArray && args.shape = [2]
-        super(args[0], args[1])
-      # else
-      #   super[args]
-      # end
+      super(args[0], args[1])
     end
     
     def self.[] *args
@@ -156,11 +168,14 @@ module HD
       r
     end
     
-    # Converts a string (separated by a tab, whitespace, or comma) to an array of HD::Ratio objects. 
-    # Example:
-    # HD::Ratio.from_s("1/1 4/3 16/7") # => Array[HD.r, HD.r(4,3), HD.r(16,7)]
+    # Converts a string (separated by a tab, whitespace, or comma) to an array
+    # of HD::Ratio objects. Example: 
+    # 
+    #   HD::Ratio.from_s("1/1 4/3 16/7") 
+    #   # => Array[HD.r, HD.r(4,3), HD.r(16,7)]
     #
-    # Can also be used to convert backward from a printed array of Ratios (i.e., to read in a file)
+    # Can also be used to convert backward from a printed array of Ratios
+    # (i.e., to read in a file)
     def self.from_s s
       ratios = s.scan(/(\d+)\/(\d+)/)
       ratios.map! {|x| Ratio[x[0].to_i, x[1].to_i] }
@@ -183,7 +198,27 @@ module HD
       r.reduce
     end
     
-    # Generates a ratio from its constituent factors (expressed as a vector of prime exponents)
+    # For each of the num and den, provides a list of exponents. Primes are
+    # only up through the size of PRIMES. This is mostly for the sake of
+    # efficiency.
+    def factors
+      exponents = ::NArray.int(PRIMES.size, 2)
+      self.each_with_index do |y, i|
+        exponents[true,i] = ::NArray.to_na(PRIMES.to_a.map do |x|
+          exp_count = 0
+          while y % x == 0
+            exp_count += 1
+            y /= x
+          end
+          exp_count
+        end)
+      end
+      # Will be an NArray of exponents (negative numbers are denominators)
+      exponents[true,0] - exponents[true,1]
+    end
+    
+    # Generates a ratio from its constituent factors (expressed as a vector of
+    # prime exponents)
     def self.from_factors factors
       (factors.is_a? Array) ? factors = ::NArray.to_na(factors) : false
       f = PRIMES ** factors.abs
@@ -212,9 +247,13 @@ module HD
       end
     end
     
+    # Multiplication and division are done by factoring the consituent
+    # dimensions of the Ratio object and adding or subtracting the exponents
+    # of the prime numbers. Because of this, any Ratio object that uses prime
+    # numbers not included in the PRIMES variable may lead to bugs. 
     def * r
       if r.is_a? Ratio
-        # Default response. All other ifs should point toward this.
+        # Default response. All elsifs should point toward this.
         Ratio.from_factors(self.factors + r.factors)
       elsif r.is_a? Numeric
         self * Ratio[r, 1]
@@ -227,7 +266,7 @@ module HD
     
     def / r
       if r.is_a? Ratio
-        # Default response. All other ifs should point toward this.
+        # Default response. All elsifs should point toward this.
         Ratio.from_factors(self.factors - r.factors)
       elsif r.is_a? Numeric
         self / Ratio[1, r]
@@ -240,8 +279,9 @@ module HD
       if r == 2
         super
       else
-        # For some reason NVector doesn't respond to ** unless the other is 2,
-        # so we have to call NArray's ** method
+        # NVector treats ** as A * A (produces a single digit), instead of the
+        # element-wise operation of ** that NArray allows. Have to call
+        # NArray's ** method for this.
         ::NArray.instance_method(:**).bind(self).call(r)
       end
     end
@@ -270,6 +310,11 @@ module HD
       return self, other
     end
     
+    # Defines the hash to properly test for equality.
+    def hash
+      [self[0], self[1]].hash
+    end
+    
     # Necesssary to test for sets and subsets
     def eql? r
       if r.is_a? Ratio
@@ -281,11 +326,7 @@ module HD
       end
     end
     
-    # Defines the hash to properly test for equality
-    def hash
-      [self[0], self[1]].hash
-    end
-    
+    # Alias for :eql?
     def == r
       self.eql? r
     end
@@ -296,7 +337,8 @@ module HD
       yield self[1]
     end
     
-    # The normal map method returns an Array (we want a Ratio back)
+    # The normal map method returns an Array, but we want a Ratio back. So
+    # this is a slightly different method.
     def map
       r = self.dup
       begin
@@ -309,46 +351,39 @@ module HD
       r
     end
     
-    # For each of the num and den, provides a list of exponents. Primes are only up through the size of PRIMES.
-    def factors
-      exponents = ::NArray.int(PRIMES.size, 2)
-      self.each_with_index do |y, i|
-        exponents[true,i] = ::NArray.to_na(PRIMES.to_a.map do |x|
-          exp_count = 0
-          while y % x == 0
-            exp_count += 1
-            y /= x
-          end
-          exp_count
-        end)
-      end
-      # Will be an NArray of exponents (negative numbers are denominators)
-      exponents[true,0] - exponents[true,1]
-    end
-    
-    # Returns the harmonic distance (as a "city block" measurement) between two points. If no second point is specified, then
-    # it is assumed that we want the distance from a 1/1 origin.  
-    # If either point in question lies outside of the harmonic
-    # space, then the distance is _Infinity_. This comes into play in a number of ways, but the most common is as a filter
-    # for the set of all tuneable intervals. Working in (for example) a 7-limit harmonic space would require that the
-    # interval of 11/4 (while a tuneable interval) would not be eligible for use. Therefore, a distance of Infinity allows
-    # this to be filtered out of the list of tuneable intervals. (See the method HDConfig#reject_untuneable_intervals! for
+    # Returns the harmonic distance (as a "city block" measurement) between
+    # two points. If no second point is specified, then it is assumed that we
+    # want the distance from a 1/1 origin.  If either point in question lies
+    # outside of the harmonic space, then the distance is _Infinity_. This
+    # comes into play in a number of ways, but the most common is as a filter
+    # for the set of all tuneable intervals. Working in (for example) a
+    # 7-limit harmonic space would require that the interval of 11/4 (while a
+    # tuneable interval) would not be eligible for use. Therefore, a distance
+    # of Infinity allows this to be filtered out of the list of tuneable
+    # intervals. (See the method HDConfig#reject_untuneable_intervals! for
     # more information).
     def distance(origin = Ratio[1,1], config = HD::HDConfig.new)
-      # Take weights from the config element passed in
+      # Take weights from the config argument
       weights = config.prime_weights
-      # Get the factors of the numerator and denominator of the interval from the point to the origin
+      # Get the factors of the interval between self and the origin arg
       factors = (self.dup / origin).factors
-      # In any reasonable sense, the weights array and factors array must be the same size
+      # The weights array and factors array must be the same size, because
+      # otherwise there's the potential of missing weights, or untraceable
+      # bugs.
       if factors.size != weights.size
         warn "Weights and factors are not the same size!" 
         puts "Factors size: #{factors.size}, Weights size: #{weights.size}"
       end
       
-      # If a factor's weight is 0 then it is outside of the harmonic space, and its distance is Infinity.
+      # If the Ratio in question contains a prime number factor whose weight
+      # has been set to 0, then it is outside of the harmonic space, and its
+      # distance is Infinity. To say that a certain prime factor "doesn't
+      # matter" (e.g., "octave equivalency"), set that factor's weight to 1.
       if ((factors.ne 0) & (weights.ne 0)) != (factors.ne 0)
         return NMath.log2(0) * -1
-      else # Calculation of the city block metric, using vectors instead of iteration
+      else 
+        # Calculation of the city block metric, using vectors instead of
+        # iteration.
         wf = weights ** factors.abs
         NMath.log2((wf).abs[wf.ne 0.0].prod)
       end
@@ -388,188 +423,7 @@ module HD
     n.to_a.to_s
   end
   
-  # The chord is essentially just a sorted set that translates some of the basic
-  # functions so that they'll work with the Ratio objects. It also will calculate
-  # the total distance between all possible points (combinatorial summation) and 
-  # can return a set of all possible pairs.
-  class Chord < SortedSet
-
-    # Iterate through and create a set of pairs, not counting pairs of the same chord
-    def pairs
-      pairs = Set.new
-      self.each do |x|
-        self.each do |y|
-          pairs << [x, y].sort
-        end
-      end
-      pairs.reject! {|x| x[0] == x[1]}
-      pairs
-    end
-    
-    # sum of the combinatorial distance of all members of the chord.
-    # the distance between each possible pair is computed, and that
-    # amount is summed to compute the total distance
-    def hd_sum(config = HD::HDConfig.new)
-      total = 0
-      all_pairs = []
-      self.pairs.each {|x,y| all_pairs << [x,y]}
-      all_pairs.each {|x| total += x[0].distance(x[1], config)}
-      total
-    end
-    
-    # See "Crystal Growth in Harmonic Space," by James Tenney
-    # as well as the tables and refinements suggested by
-    # Marc Sabat & Wolfgang von Schweinitz (plainsound.org)
-    #
-    # Finds the point which is the least possible harmonic distance
-    # from all other points, while not being a member of the chord
-    # itself. Should have the smallest possible return of 
-    # total_distance when added to the chord.
-    #
-    # 1. Look through all tuneable intervals for each member
-    # 2. Add each one to the chord, and evaluate total_distance
-    # 3. Pick the interval with the least total_distance
-    def logical_origin(config = HDConfig.new)
-      least_harmonic_distance = {:distance => nil, :ratio => nil}
-      self.each do |m|
-        config.tuneable.each do |i|
-          if self.member? m * i
-            next
-          end
-          if (m * i) > Ratio.new(9,2) || (m * i) < Ratio.new(4,9)
-            next
-          end
-          c = self.dup
-          c << m * i
-          if (least_harmonic_distance[:ratio] == nil)
-            least_harmonic_distance[:ratio] = m * i
-            least_harmonic_distance[:distance] = c.hd_sum(config)
-          elsif (least_harmonic_distance[:distance] > c.hd_sum(config))
-            least_harmonic_distance[:ratio] = m * i
-            least_harmonic_distance[:distance] = c.hd_sum(config)
-          end
-        end
-      end
-      least_harmonic_distance
-    end
-    
-    # Returns an array of all possible candidates connected to the pitches
-    # Optional argument pc_only allows for consideration of only pitch-class
-    # projection space (where octave equivalency is respected)
-    def candidates(config = HD::HDConfig.new)
-      candidates = []
-      self.each do |e|
-        PRIMES.each do |p|
-          # Add each connected element
-          candidates << e * Ratio.new(p, 1)
-          candidates << e * Ratio.new(1, p)
-        end
-      end
-      # Filter everything for pitch-class space
-      if config.pc_only
-        candidates.each do |x|
-          x = x.pc_space
-        end
-        candidates = candidates & candidates
-      end
-      candidates.reject! {|x| self.include? x}
-      candidates
-    end
-    
-    def to_s
-      str = ""
-      self.each {|x| str << x}
-    end
-  end # Chord (Class)
-  
-  class WeightedArray < Array
-    def initialize(*x)
-      if x
-        super(x)
-      else
-        super
-      end
-      @weights = Array.new(self.size, 1)
-    end
-  
-    def choose
-      normalized = []
-      # Normalize (divide each weight by the sum of all weights)
-      sum = 0.0
-      @weights.each {|x| sum += x}
-      if sum == 0.0
-        raise "WTF SUM IS ZERO"
-      end
-      normalized = @weights.map {|x| x /= sum }
-      normalized.each_with_index {|x,i| x += normalized[i-1] unless i == 0}      
-      # Each item should equal itself plus the previous item      
-      ranking = Array.new(normalized.size)
-      normalized.each_index do |i| 
-        if i == 0
-          ranking[i] = normalized[i]
-        else
-          ranking[i] = ranking[i-1] + normalized[i]
-        end
-      end
-      begin
-        r = rand
-        chosen = 0
-        
-        while r >= ranking[chosen]
-          chosen += 1
-        end
-      rescue ArgumentError => er
-        print "#{er.message}\n#{r}\t#{ranking}"
-      end
-      
-      return self[chosen]
-    end
-  
-    # Sets the new weighting
-    def weights=(input_weights)
-      if !(input_weights.instance_of? Array)
-        return nil
-      end
-      input_weights.map! {|x| x.to_f}
-      @weights = input_weights if input_weights.size == self.size
-    end
-  
-    def weights
-      return @weights
-    end
-  end
-  
-  # A series of functions for choosing new pitches.
-  
-  #
-  # Select: Provide this thing with a base ratio and a config file (with prime weights 
-  # & tuneable intervals) and it'll choose a new pitch based on that prime number weighting.
-  # Supremely useful for finding the next interval to use based on harmonic distance, without
-  # being entirely deterministic. Essentially creates a distribution of probabilities to shoot for
-  # 
-  # Ideas: r could be the current origin at all times. So that would create a certain cloud
-  # around a particular pitch. When that cloud encroaches on another origin, the cloud
-  # may change its origin. That's left for a different controller module.
-  #
-  
-  def self.select(r = Ratio.new, config = HDConfig.new)
-    intervals = config.tuneable.map {|x| r * x}
-    intervals = WeightedArray.new(*intervals)
-    puts "#{intervals}"
-    intervals.weights = intervals.map do |x| 
-      if x.distance == 0.0
-        0.0
-      else
-        1.0 / x.distance
-      end
-    end
-    puts "#{intervals.weights}"
-    intervals.choose
-  end
-  
-  #
-  # Shorthand: Outside of the class, HD.r(n, m) is short for HD::Ratio.new(n, m)
-  #
+  # Outside of the class, HD.r(n, m) is short for HD::Ratio[n, m]
   class_eval(<<-EOS, __FILE__, __LINE__)
     def self.r(n = 1, m = 1)
       Ratio[n, m]
