c0de3230f0b348ead78672d2dd877c9de8f113b0
Andrew Smith <andrewchristophersmith@gmail.com>
2013-03-17 16:07:21 -0400

    reorganized

--- search/MetricSearch.rb
+++ search/MetricSearch.rb
@@ -11,41 +11,7 @@ module MM
 			@epsilon						= opts[:epsilon]					|| 0.01
 			@max_iterations			= opts[:max_iterations]		|| 1000
 			@goal_vector				= opts[:goal_vector]			|| (raise ArgumentError, "opts[:goal_vector] required")
-		end
-		
-		def prepare_search
-			@path = []
-			@current_point = @start_vector
-			@interval_index = 0
-		end
-		
-		def get_cost_vector
-			# Null for the dummy class
-			# Must define in subclass
-			raise "Must define :get_cost_vector in MM::MetricSearch"
-		end
-		
-		def prepare_result
-			# Null for the dummy class
-			# Defining in subclass is optional
-		end
-		
-		def debug_each_iteration iter
-			case @debug_level
-			when > 1 # Prints out a play-by-play
-				puts "Iteration #{iter}"
-				puts "Now #{@current_cost} away at #{@current_point.to_a}"
-			when > 0
-        # Tells us where we are with each large-scale movement
-        print "\t\t\t\t\rIteration #{iter}: #{@current_cost} away at #{@current_point.to_a}"
-			end
-		end
-		
-		def prepare_each_run
-			if @initial_run
-				@interval_index = 0
-				@initial_run = false
-			end
+			@banned_points			= opts[:banned_points]		|| {}
 		end
 		
 		def search
@@ -65,18 +31,18 @@ module MM
 										throw :jump_back
 									end
 									possible_point = get_possible_point(cost_vector, @interval_index)
-									while (@banned_points.has_key? HD.narray_to_string possible_point) || (@path.include? possible_point)
+									while (@banned_points.has_key? possible_point.hash) || (@path.include? possible_point)
 										@interval_index += 1
 										# If we've exhausted all possible intervals
 										if @interval_index >= cost_vector.size
-											@banned_points[HD.narray_to_string possible_point] = 1
+											@banned_points[possible_point.hash] = 1
 											@initial_run = true
 											throw :jump_back
 										end
 										# Get the index of the movement that is at the current index level
 										possible_point = get_possible_point(@cost_vector, @interval_index)
 									end
-									if @banned_points.has_key? HD.narray_to_string possible_point
+									if @banned_points.has_key? possible_point.hash
 										throw :jump_back
 									end
 									@path << possible_point
@@ -87,37 +53,30 @@ module MM
 									initial_run = true
 									throw :jump_back
 								end
-								
+								# Find coordinates of the current point
 								begin
 									current_coordinates = @get_coords.(@path[-1], @start_vector)
 								rescue RangeError => er
-									handle_range_error
-									raise er
+									handle_range_error(er) ? retry : raise er
 								end
-								
+								# Find the cost of the prospective point
 								prospective_cost = get_cost(current_coordinates, @goal_vector)
 								if prospective_cost < @current_cost
 									@current_cost = prospective_cost
 								else
 									throw :jump_back
 								end
-								
+								# If the current cost is less than the goal, success!
 								if @current_cost < @epsilon
 									@current_point = @path[-1]
 									prepare_result
 	                throw :success
-								else # Advance to the next without jumping back
+								else # Otherwise, advance to the next iteration without jumping back
 									initial_run = true
 									throw :keep_going
 								end
 							end # catch :jump_back
-							@banned_points[HD.narray_to_string @path[-1]] = @path.pop
-							@current_point = @path[-1] || (@path << @start_vector)[-1]
-							@current_cost = get_cost(@get_coords.(@current_point, @start_vector), @goal_vector)
-							@banned_points.delete HD.narray_to_string @start_vector
-							if @debug_level > 1
-								puts "banning #{@banned_points[-1].to_a}"
-							end
+							jump_back
 						end # catch :keep_going
 					rescue RuntimeError => er
 						puts "\n#{er.message}"
@@ -127,25 +86,95 @@ module MM
 					end # RuntimeError block
 				end # main iteration loop
 			end # catch :success
-			if debug_level > 0
+			debug_final_report
+			prepare_data
+			[@current_point, @data]
+		end
+		
+		def prepare_search
+			@current_point = @start_vector
+			@interval_index = 0
+			@current_cost = get_cost(@get_coords.(@current_point, @start_vector), goal_vector)
+			@best_point_so_far = @current_point
+			@best_cost_so_far = @current_cost
+			@path = [@start_vector]
+			@initial_run = true
+		end
+		
+		def get_cost
+			# Null for the dummy class
+			raise "Must define :get_cost in subclass of MM::MetricSearch"
+		end
+		
+		def get_cost_vector
+			# Null for the dummy class
+			# Must define in subclass
+			raise "Must define :get_cost_vector in subclass of MM::MetricSearch"
+		end
+		
+		# Gets a point from indices
+		def get_possible_point(cost, interval_index)
+			ind_x, ind_y = sort_by_cost(@cost_vector, @interval_index)
+			HD.change_inner_interval(@current_point, ind_y, HD.r(*@tuneable[ind_x]))
+		end
+		
+		def prepare_result
+			# Null for the dummy class
+			# Defining in subclass is optional
+		end
+		
+		def handle_range_error er
+			# Dummy method
+		end
+		
+		def debug_each_iteration iter
+			case @debug_level
+			when > 1 # Prints out a play-by-play
+				puts "Iteration #{iter}"
+				puts "Now #{@current_cost} away at #{@current_point.to_a}"
+			when > 0
+        # Tells us where we are with each large-scale movement
+        print "\t\t\t\t\rIteration #{iter}: #{@current_cost} away at #{@current_point.to_a}"
+			end
+		end
+		
+		def debug_final_report
+			case @debug_level
+			when > 0
 				puts "\nSuccess at: \t#{@current_point.to_a}"
-	      puts "Lowest OLD: \t#{@lowest_old[0].to_a}"
-	      puts "Angle: \t\t#{@get_angle.(@current_point, @start_vector)}"
 	      puts "Distance: \t#{get_cost(@get_coords.(@current_point), 0)}"
 	      puts "Cost: \t\t#{@current_cost}"
 			end
+		end
+		
+		def jump_back
+			@banned_points[@path[-1].hash] = @path.pop
+			@current_point = @path[-1] || (@path << @start_vector)[-1]
+			@current_cost = get_cost(@get_coords.(@current_point, @start_vector), @goal_vector)
+			@banned_points.delete @start_vector.hash
+			if @debug_level > 1
+				puts "banning #{@banned_points[-1].to_a}"
+			end
+		end
+		
+		def prepare_each_run
+			if @initial_run
+				@interval_index = 0
+				@initial_run = false
+			end
+		end
+		
+		def prepare_data
 			@data = {
-				:tuneable_data => @tuneable_data,
 				:banned_points => @banned_points,
 				:cost => @current_cost,
-				:lowest_old => @lowest_old,
 				:path => @path
 			}
 			if @current_cost > @epsilon
 				@data[:failed] = true
 				@current_point = @best_point_so_far
 				@current_cost = get_cost(@get_coords.(@current_point, @start_vector), @goal_vector)
-			[@current_point, @data]
+			end
 		end
 	end
   
@@ -159,12 +188,12 @@ module MM
       @is_scaled?					= opts[:is_scaled]				|| false
 			@tuning_range				= opts[:tuning_range]			|| [HD.r(2,3), HD.r(16,1)]
       # Reference methods from the angler
-			if is_scaled?
-				@get_coords = angler.method(:get_scaled_coordinates_from_reference)
-				@get_angle = angler.method(:get_scaled_angle)
+			if @is_scaled?
+				@get_coords 			= angler.method(:get_scaled_coordinates_from_reference)
+				@get_angle 				= angler.method(:get_scaled_angle)
 			else
-				@get_coords = angler.method(:get_coordinates_from_reference)
-				@get_angle = angler.method(:get_angle)
+				@get_coords 			= angler.method(:get_coordinates_from_reference)
+				@get_angle 				= angler.method(:get_angle)
 			end
 			# Load list of tuneable intervals, reject those that won't work
 			@hd_config.reject_untuneable_intervals!
@@ -173,161 +202,47 @@ module MM
 			@tuneable = hd_config.tuneable
       
 			@debug_level > 0 ? (print "\n== Getting tuneable data...") : false
-	    @tuneable_data			= opts[:tuneable_data]		|| get_tuneable_data(NArray.to_na(start_vector), get_coords, hd_config)
+	    @tuneable_data			= opts[:tuneable_data]		|| get_tuneable_data(NArray.to_na(@start_vector), @get_coords, @hd_config)
 	    @debug_level > 0 ? (puts "done.") : false
-	    @banned_points			= opts[:banned_points]		|| {}
-			
 			@lowest_old = []
     end
-		
-		def prepare_search
-			super
-			@path = []
-			@current_cost = get_cost(@get_coords.(@current_point, @start_vector), goal_vector)
-			@best_point_so_far = @current_point
-			@best_cost_so_far = @current_cost
-			@path << @start_vector
-			@initial_run = true
-		end
-		
-		# Makes sure that the current point is satisfactory
-		def prepare_result
-			@lowest_old = MM.get_lowest_old(@current_point, @start_vector, @hd_config, false, @tuning_range)
-      if @lowest_old[0] == nil
-				@initial_run = true
-				throw :jump_back
-			end
-		end
-		
-		def search
-			interval_index = 0
-			catch :success do
-				@max_iterations.times do |iter|
-					begin # RuntimeError block
-						
-						case @debug_level
-						when > 1 # Prints out a play-by-play
-							puts "Iteration #{iter}"
-							puts "Now #{@current_cost} away at #{@current_point.to_a}"
-						when > 0
-	            # Tells us where we are with each large-scale movement
-	            print "\t\t\t\t\rIteration #{iter}: #{@current_cost} away at #{current_point.to_a}"
-						end
-						catch :keep_going do
-							catch :jump_back do
-								# Generate a table of all possible costs
-								# Cost vector is summation of all possible movements
-								cost = NMath.sqrt(((@tuneable_data - @goal_vector) ** 2).sum(0))
-								if initial_run
-									interval_index = 0
-									initial_run = false
-								end
-								begin # IndexError block
-									if interval_index >= cost.size
-										throw :jump_back
-									end
-									possible_point = get_possible_point(cost, interval_index
-									while (banned_points.has_key? HD.narray_to_string possible point) || (path.include? possible_point)
-										interval_index += 1
-										# If we've exhausted all possible intervals
-										if interval_index >= cost.size
-											banned_points[HD.narray_to_string possible_point] = 1
-											initial_run = true
-											throw :jump_back
-										end
-										# Get the index of the movement that is at the current index level
-										possible_point = get_possible_point(cost, interval_index)
-									end
-									(@banned_points.has_key? HD.narray_to_string possible_point)
-								rescue IndexError => er
-									puts "\nIndexError: #{er.message}"
-									print er.backtrace.join("\n")
-									initial_run = true
-									throw :jump_back
-								end
-								
-								begin
-									current_coordinates = get_coords.(path[-1], start_vector)
-								rescue RangeError => er
-									puts "\nSeem to have a RangeError -- reordering"
-									inner_v = vector_delta(@path[-1], 1, get_inner_interval_delta(@hd_config), INTERVAL_FUNCTIONS[:pairs])
-	                ((inner_v.shape[1]/2)...inner_v.shape[1]).times do |x|
-	                  inner_v[true,x] = NArray[inner_v[true,x][1], inner_v[true,x][0]]
-	                end
-	                path[-1] = vector_from_differential inner_v
-	                puts "#{path[-1].to_a}"
-	                retry
-								end
-								
-								prospective_cost = get_cost(current_coordinates, goal_vector)
-								if prospective_cost < @current_cost
-									@current_cost = prospective_cost
-								else
-									throw :jump_back
-								end
-								
-								if @current_cost < @epsilon
-									@current_point = @path[-1]
-									@lowest_old = MM.get_lowest_old(@current_point, @start_vector, @hd_config, false, @tuning_range)
-	                if @lowest_old[0] == nil
-										initial_run = true
-										throw :jump_back
-									end
-	                throw :success
-								else # Advance to the next without jumping back
-									initial_run = true
-									throw :keep_going
-								end
-							end # catch :jump_back
-							@banned_points[HD.narray_to_string @path[-1]] = @path.pop
-							@current_point = @path[-1] || (@path << @start_vector)[-1]
-							@current_cost = get_cost(@get_coords.(@current_point, @start_vector), @goal_vector)
-							@banned_points.delete HD.narray_to_string @start_vector
-							if @debug_level > 1
-								puts "banning #{@banned_points[-1].to_a}"
-							end
-						end # catch :keep_going
-					rescue RuntimeError => er
-						puts "\n#{er.message}"
-						print er.backtrace.join("\n")
-						print "\n\nPath: #{@path.to_a}\n\n\n"
-						break
-					end # RuntimeError block
-				end # main iteration loop
-			end # catch :success
-			if debug_level > 0
-				puts "\nSuccess at: \t#{@current_point.to_a}"
-	      puts "Lowest OLD: \t#{@lowest_old[0].to_a}"
-	      puts "Angle: \t\t#{@get_angle.(@current_point, @start_vector)}"
-	      puts "Distance: \t#{get_cost(@get_coords.(@current_point), 0)}"
-	      puts "Cost: \t\t#{@current_cost}"
-			end
-			@data = {
-				:tuneable_data => @tuneable_data,
-				:banned_points => @banned_points,
-				:cost => @current_cost,
-				:lowest_old => @lowest_old,
-				:path => @path
-			}
-			if @current_cost > @epsilon
-				@data[:failed] = true
-				@current_point = @best_point_so_far
-				@current_cost = get_cost(@get_coords.(@current_point, @start_vector), @goal_vector)
-			[@current_point, @data]
-		end
 		
 		####################
 		## HELPER METHODS ##
 		####################
+		# Tuneable data method
+	  def self.get_tuneable_data(origin, get_coords, hd_config)
+	    # Initialize an empty NArray with all data
+	    tuneable_data = NArray.float(2, hd_config.tuneable.size, origin.shape[1]-1)
+	    # TODO: Vectorize these loops!
+	    # Iterate through each first differential index
+	    # i = index of the first differential
+	    tuneable_data.shape[2].times do |i| 
+	      # Iterate through each tuneable interval
+	      # j = index of each tuneable interval
+	      tuneable_data.shape[1].times do |j|
+	        begin
+	          # find the first differential of the vector
+	          vector_delta = (MM.vector_delta(origin, 1, MM::DELTA_FUNCTIONS[:hd_ratio], MM::INTERVAL_FUNCTIONS[:pairs])).dup
+	          # switch out each of the intervals in the first differential with
+	          # one of the tuneable intervals
+	          vector_delta[true,i] = hd_config.tuneable[j]
+	          # log how the vector moved from the origin
+	          tuneable_data[true,j,i] = get_coords.(MM.vector_from_differential(vector_delta), origin)
+	        # rescue
+	        #   puts "index #{j}, #{i} didn't seem to work"
+	        end
+	      end
+	    end
+	    tuneable_data 
+	  end
+		# Cost function for this method
+		def get_cost(current_coordinates, goal_vector)
+			NMath.sqrt(((current_coordinates - goal_vector) ** 2).sum)
+		end
 		# Fills in the cost vector
 		def get_cost_vector
 			NMath.sqrt(((@tuneable_data - @goal_vector) ** 2).sum(0))
-		end
-		# Gets a point from indices
-		def get_possible_point(cost, interval_index)
-			ind_x, ind_y = sort_by_cost(cost, interval_index)
-			possible_point = HD.change_inner_interval(@current_point, ind_y, HD.r(*@tuneable[ind_x]))
-			possible_point
 		end
 		# Takes care of the RangeError and retries
 		def handle_range_error
@@ -338,11 +253,30 @@ module MM
       end
       @path[-1] = vector_from_differential inner_v
       puts "#{@path[-1].to_a}"
-      retry
+      true # Error handled
 		end
-		# Cost function for this method
-		def get_cost(current_coordinates, goal_vector)
-			NMath.sqrt(((current_coordinates - goal_vector) ** 2).sum)
+		# Makes sure that the current point is satisfactory
+		def prepare_result
+			super
+			@lowest_old = MM.get_lowest_old(@current_point, @start_vector, @hd_config, false, @tuning_range)
+      if @lowest_old[0] == nil
+				@initial_run = true
+				throw :jump_back
+			end
+		end
+		# Prepare the final data report, specific to OLM tuning
+		def prepare_data
+			super
+			@data[:tuneable_data] => @tuneable_data
+			@data[:@lowest_old] => @lowest_old
+		end
+		# Send the final report
+		def debug_final_report
+			super
+			if debug_level > 0
+	      puts "Lowest OLD: \t#{@lowest_old[0].to_a}"
+	      puts "Angle: \t\t#{@get_angle.(@current_point, @start_vector)}"
+			end
 		end
   end
 end
